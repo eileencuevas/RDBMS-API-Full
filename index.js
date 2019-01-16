8badb1282d02b7750885918bdbcd5a47556195e1
steelworx <eileencuevas62@gmail.com>
2019-01-16 17:11:29 -0500

    adde GET endpoints for cohorts table in index.js

--- index.js
+++ index.js
@@ -6,4 +6,46 @@ const knexConfig = require('./knexfile');
 const server = express();
 const db = knex(knexConfig.development);
 
+server.use(express.json());
+
+server.get('/api/cohorts/:id/students', (req, res) => {
+    db('students')
+        .where({ cohort_id: req.params.id })
+        .then(students => {
+            if (students.length > 0) {
+                res.status(200).json(students);
+            } else {
+                res.status(404).json({ error: 'No Students found for that Cohort ID.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not load students or cohorts. Try again.' });
+        });
+})
+
+server.get('/api/cohorts/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .then(cohort => {
+            if (cohort.length > 0) {
+                res.status(200).json(cohort);
+            } else {
+                res.status(404).json({ error: 'There was no Cohort found with that ID.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not load cohorts. Try again.' });
+        });
+})
+
+server.get('/api/cohorts/', (req, res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json(cohorts);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Could not load cohorts. Try again.' });
+        });
+})
+
 server.listen(5000, () => console.log('server running on 5000'));
